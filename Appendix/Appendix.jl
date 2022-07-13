08f50a8b42bb8d2f2288149f50300c596b6131e1
Christoph Aurnhammer <chr.aurnhammer@gmail.com>
2022-07-13 14:14:45 +0200

    Appendix models from rERP primer

---
+++ Appendix/Appendix.jl
@@ -0,0 +1,79 @@
+
+# Christoph Aurnhammer, 2022
+# This is the inital version of my rERP.jl primer.
+# rERPs have initially been described by Smith and Kutas (2015a, 2015b).
+# DO NOT DISTRIBUTED THIS CODE.
+# A final version will be made public as part of my Dissertation.
+
+# Load the functions in rERPs.jl
+# Packages are loaded from within rERPs.jl
+include("../code/rERP.jl");
+
+# Define an array of electrodes on which to fit the models.
+elec = [:Fp1, :Fp2, :F7, :F3, :Fz, :F4, :F8, :FC5, :FC1, :FC2, :FC6, :C3,
+        :Cz, :C4, :CP5, :CP1, :CP2, :CP6, :P7, :P3, :Pz, :P4, :P8, :O1, :Oz, :O2];
+
+# Define a models Structure. All arguments are arrays of column name Symbols, e.g. [:Item, :Subject]
+# Arguments:
+# - Descriptors: Fit separate models for each of these. You DO NOT have to add electrodes here!
+# - NonDescriptors: Columns that we don't use but want to carry over to our output data.
+# - Electrodes: Electrodes on which to fit separate models.
+# - Predictors: Predictors to use in the model.
+models = make_models([:Subject, :Timestamp], [:Item, :Condition], elec, [:Intercept]);
+
+# Pre-process the data, using the following arguments:
+# - infile: Path to input file. Necessary.
+# - outfile: Path to output file. set to false to return DataFrame directly instead of writing. Necessary.
+# - models: Models structure specified above. Necessary.
+# - baseline_corr: Apply baseline correction, if you haven't done so already. Default false.
+# - sampling_rate: Downsample to a lower sampling rate. Default false.
+# - invert_preds: Takes an array of predictor Symbols (e.g., [:Cloze, :Association]) to invert. Default false.
+# - conds: Takes an array of condition labels to subset to (e.g. ["A", "B"]). Default false.
+# - components: Collect the average N400 and Segment amplitude (Special sauce for one of my projects). Default false.
+@time process_data("../data/CAPExp.csv", "/data/CAPExp_AC_rERP.csv", models, conds=["A", "C"]);
+
+# Read in processed data.
+@time dt = read_data("/data/CAPExp_AC_rERP.csv", models);
+
+# Fit the rERP models, using the three arguments:
+# - data: data as processed by process_data()
+# - models: models Structure
+# - file: path to output file. Filenames will be automatically extended to *_data.csv and *_models.csv.
+@time fit_models(dt, models, "/data/CAPExp_AC_rERP");
+
+##########################
+# rERPs as ERP averaging #
+##########################
+##### Fit intercept only models = compute grand-averaged ERP
+models = make_models([:Subject, :Timestamp], [:Item, :Condition], elec, [:Intercept]);
+@time process_data("../data/CAPExp.csv", "/data/CAPExp_AC_rERP.csv", models, conds=["A", "C"]);
+@time dt = read_data("/data/CAPExp_AC_rERP.csv", models);
+@time fit_models(dt, models, "/data/CAPExp_AC_Intercept_rERP");
+
+##### Fit A vs. C condition coding models = compute per-condition grand-averaged ERP
+# Create A vs C condition coding predictor. B and D rows are going to be discarded.
+cond_coding = Dict("A" => -0.5, "B" => 0, "C" => 0.5, "D" => 0);
+dt = DataFrame(File("../data/CAPExp.csv"));
+dt.CondCode = [cond_coding[x] for x in dt.Condition];
+write("../data/CAPExp.csv", dt);
+
+models = make_models([:Subject, :Timestamp], [:Item, :Condition], elec, [:Intercept, :CondCode]);
+@time process_data("../data/CAPExp.csv", "/data/CAPExp_AC_rERP.csv", models, conds=["A", "C"]);
+@time dt = read_data("/data/CAPExp_AC_rERP.csv", models);
+@time fit_models(dt, models, "/data/CAPExp_AC_CondCode_rERP");
+
+########################
+# Continous predictors #
+########################
+# Fit models on conditions A and C using cloze probability
+models = make_models([:Subject, :Timestamp], [:Item, :Condition], elec, [:Intercept, :Cloze]);
+@time process_data("../data/CAPExp.csv", "/data/CAPExp_AC_cloze_rERP.csv", models, invert_preds=[:Cloze], conds=["A", "C"]);
+@time dt = read_data("/data/CAPExp_AC_cloze_rERP.csv", models);
+@time fit_models(dt, models, "/data/CAPExp_AC_cloze_rERP");
+
+# Fit models on all conditions using cloze probability and association
+# In order to not take any condition subset, set conds = false.
+models = make_models([:Subject, :Timestamp], [:Item, :Condition], elec, [:Intercept, :Cloze, :rcnoun]);
+@time process_data("../data/CAPExp.csv", "/data/CAPExp_cloze_rcnoun_rERP.csv", models, invert_preds=[:Cloze]);
+@time dt = read_data("/data/CAPExp_cloze_rcnoun_rERP.csv", models);
+@time fit_models(dt, models, "/data/CAPExp_cloze_rcnoun_rERP");
---
+++ Appendix/Appendix_plot.r
@@ -0,0 +1,123 @@
+source("../code/plot_rERP.r")
+source("../code/benjamini-hochberg.r")
+
+make_plots <- function(
+    file,
+    elec = c("F3", "Fz", "F4", "C3", "Cz", "C4", "P3", "Pz", "P4"),
+    predictor = "Intercept"
+) {
+    # make dirs
+    system(paste0("mkdir ../plots/", file))
+    system(paste0("mkdir ../plots/", file, "/Waveforms"))
+    system(paste0("mkdir ../plots/", file, "/Topos"))
+
+    ##########
+    # MODELS #
+    ##########
+    mod <- fread(paste0("../data/", file, "_models.csv"))
+    mod$Spec <- factor(mod$Spec, levels = predictor)
+
+    # Models: coefficent
+    coef <- mod[Type == "Coefficient", ]
+    coef$Condition <- coef$Spec
+
+    model_labs <- predictor
+    model_vals <- c("black", "#E349F6", "#00FFFF")
+    plot_elec(data = coef, e = elec, file = paste0("../plots/", file,
+        "/Waveforms/Coefficients.pdf"), modus = "Coefficient",
+        ylims = c(10, -5.5), leg_labs = model_labs, leg_vals = model_vals)
+    # plot_topo(coef, file = paste0("../plots/", file, "/Topos/Coefficients"),
+    #             tw = c(300, 500), cond_man = predictor[2],
+    #             cond_base = "Intercept", label = "Coefficient")
+
+    # Models: t-value
+#     time_windows <- list(c(300, 500), c(600, 1000))
+#     tval <- mod[Type == "t-value" & Spec != "Intercept", ]
+#     sig <- mod[Type == "p-value" & Spec != "Intercept", ]
+#     colnames(sig) <- gsub("_CI", "_sig", colnames(sig))
+
+#     sig_corr <- bh_apply_wide(sig, elec, alpha = 0.05, tws = time_windows)
+
+#     sigcols <- grepl("_sig", colnames(sig_corr))
+#     tval <- cbind(tval, sig_corr[,..sigcols])
+#     tval$Condition <- tval$Spec
+#     plot_elec(tval, elec, 
+#         file = paste0("../plots/", file, "/Waveforms/t-values.pdf"),
+#         modus = "t-value", ylims=c(7, -5), tws=time_windows,
+#         leg_labs = model_labs[2:length(model_labs)],
+#         leg_vals = model_vals[2:length(model_vals)])
+
+#     plot_topo(tval, file = paste0("../plots/", file, "/Topos/t-values"),
+#                 tw = c(300, 500), cond_man = predictor,
+#                 cond_base = "Intercept", label = "Coefficient")
+
+    ########
+    # DATA #
+    ########
+    eeg <- fread(paste0("../data/", file, "_data.csv"))
+
+    # eeg$Condition <- factor(plyr::mapvalues(eeg$Condition, c(1, 2),
+    #                 c("A", "C")), levels = c("A", "C"))
+    # data_labs <- c("A", "C")
+    # data_vals <- c("black", "#004488")
+    
+    eeg$Condition <- factor(plyr::mapvalues(eeg$Condition, c(4, 1, 3, 2),
+                     c("B", "A", "C", "D")), levels = c("A", "B", "C", "D"))
+    data_labs <- c("A", "B", "C", "D")
+    data_vals <- c("#000000", "#BB5566", "#004488", "#DDAA33")
+
+    # Data: Observed
+    obs <- eeg[Type == "EEG", ]
+    source("plot_rERP.r")
+    plot_elec(obs, elec,
+        file = paste0("../plots/", file,  "/Waveforms/Observed.pdf"),
+        modus = "Condition", ylims=c(8, -5.5),
+        leg_labs = data_labs, leg_vals = data_vals)
+
+    plot_topo(obs, file = paste0("../plots/", file, "/Topos/Observed"),
+                tw = c(300, 500), cond_man = "C", cond_base = "A")
+
+    # Data: Estimated
+    source("plot_rERP.r")
+    est <- eeg[Type == "est",]
+    for (i in seq(1, length(unique(est$Spec)))) {
+        spec <- unique(est$Spec)[i]
+        est_set <- est[Spec == spec, ]
+        spec <- unique(est_set$Spec)
+        name <- gsub("\\[|\\]|:|,| ", "", spec)
+        plot_elec(est_set, elec,
+                file = paste0("../plots/", file,
+                "/Waveforms/Estimated_", name, ".pdf"),
+                modus = "Condition", ylims = c(8, -5.5),
+                leg_labs = data_labs, leg_vals = data_vals)
+        plot_topo(est_set, file = paste0("../plots/",
+                file, "/Topos/Estimated_", name),
+                tw = c(300, 500), cond_man = "C", cond_base = "A")
+    }
+
+    # Data: Residual
+    source("plot_rERP.r")
+    res <- eeg[Type == "res", ]
+    for (i in seq(1, length(unique(res$Spec)))) {
+        spec = unique(res$Spec)[i]
+        res_set <- res[Spec == spec, ]
+        spec = unique(res_set$Spec)
+        name <- gsub("\\[|\\]|:|,| ", "", spec)
+        plot_elec(res_set, elec,
+                file = paste0("../plots/", file, "/Waveforms/Residual_",
+                name, ".pdf"), modus = "Condition", ylims = c(4.7, -4),
+                leg_labs = data_labs, leg_vals = data_vals)
+        plot_topo(res_set, 
+                file = paste0("../plots/", file, "/Topos/Residual_", name),
+                tw = c(300, 500), cond_man = "C", cond_base = "A")
+    }
+}
+
+
+make_plots("CAPEXP_AC_Intercept_rERP", predictor = c("Intercept"))
+
+# make_plots("CAPEXP_AC_CondCode_rERP", predictor = c("Intercept", "CondCode"))
+
+# make_plots("CAPEXP_AC_cloze_rERP", predictor = c("Intercept", "Cloze"))
+
+# make_plots("CAPEXP_cloze_rcnoun_rERP", predictor = c("Intercept", "Cloze", "rcnoun"))
