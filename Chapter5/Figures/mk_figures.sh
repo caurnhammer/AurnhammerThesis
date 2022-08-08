f3c240151738928f3802da54f785428af75f5a02
caurnhammer <chr.aurnhammer@gmail.com>
2022-08-08 15:55:24 +0200

    Ch5 code and plots woip

---
+++ Chapter5/Figures/mk_figures.sh
@@ -0,0 +1,6 @@
+mkdir final_pdf/
+
+cp ../ERP/plots/Subtration_RawN400_Quantiles.pdf final_pdf/ERP_Subtraction_Raw.pdf
+cp ../ERP/plots/Subtration_N400minusSegment_Quantiles.pdf final_pdf/ERP_Subtraction_minusSegment.pdf
+
+cp ../ERP/plots/ERP_Design1_N400Segment/Waveforms/Coefficients_Cz.pdf final_pdf/ERP_Interdependence_Coefs.pdf
