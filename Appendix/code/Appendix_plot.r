6ff618e0d75d2b92bf3c40aaa14ad3e32651be6a
Christoph Aurnhammer <chr.aurnhammer@gmail.com>
2022-09-22 17:28:39 +0200

    t-values design1 + RT modeling

--- Appendix/code/Appendix_plot.r
+++ Appendix/code/Appendix_plot.r
@@ -4,12 +4,17 @@ source("../../code/benjamini-hochberg.r")
 make_plots <- function(
     file,
     elec = c("F3", "Fz", "F4", "C3", "Cz", "C4", "P3", "Pz", "P4"),
-    predictor = "Intercept"
+    predictor = "Intercept",
+    inferential = FALSE,
+    design,
+    model_labs
 ) {
     # make dirs
     system(paste0("mkdir ../plots/", file))
     system(paste0("mkdir ../plots/", file, "/Waveforms"))
     system(paste0("mkdir ../plots/", file, "/Topos"))
+
+    elec_nine <- c("F3", "Fz", "F4", "C3", "Cz", "C4", "P3", "Pz", "P4")
 
     ##########
     # MODELS #
@@ -20,11 +25,10 @@ make_plots <- function(
     # Models: coefficent
     coef <- mod[Type == "Coefficient", ]
     coef$Condition <- coef$Spec
-    model_labs <- c("Intercept", "Cloze", "Noun Association")
     model_vals <- c("black", "#E349F6", "#00FFFF")
 
-    plot_single_elec(data = coef, e = c("Pz"), file = paste0("../plots/", file,
-        "/Waveforms/Coefficients.pdf"),
+    plot_single_elec(data = coef, e = c("Pz"), 
+        file = paste0("../plots/", file, "/Waveforms/Coefficients.pdf"),
         title = "Coefficients", modus = "Coefficient",
         ylims = c(9, -5.5), leg_labs = model_labs, leg_vals = model_vals)
 
@@ -35,48 +39,50 @@ make_plots <- function(
         ylims = c(7, -5), leg_labs = model_labs, leg_vals = model_vals)
 
     # Models: t-value
-#     time_windows <- list(c(300, 500), c(600, 1000))
-#     tval <- mod[Type == "t-value" & Spec != "Intercept", ]
-#     sig <- mod[Type == "p-value" & Spec != "Intercept", ]
-#     colnames(sig) <- gsub("_CI", "_sig", colnames(sig))
+    if (inferential == TRUE) {
+        time_windows <- list(c(300, 500), c(600, 1000))
+        tval <- mod[Type == "t-value" & Spec != "Intercept", ]
+        sig <- mod[Type == "p-value" & Spec != "Intercept", ]
+        colnames(sig) <- gsub("_CI", "_sig", colnames(sig))
 
-#     sig_corr <- bh_apply_wide(sig, elec, alpha = 0.05, tws = time_windows)
-
-#     sigcols <- grepl("_sig", colnames(sig_corr))
-#     tval <- cbind(tval, sig_corr[,..sigcols])
-#     tval$Condition <- tval$Spec
-#     plot_elec(tval, elec, 
-#         file = paste0("../plots/", file, "/Waveforms/t-values.pdf"),
-#         modus = "t-value", ylims=c(7, -5), tws=time_windows,
-#         leg_labs = model_labs[2:length(model_labs)],
-#         leg_vals = model_vals[2:length(model_vals)])
-
-#     plot_topo(tval, file = paste0("../plots/", file, "/Topos/t-values"),
-#                 tw = c(300, 500), cond_man = predictor,
-#                 cond_base = "Intercept", label = "Coefficient")
+        sig_corr <- bh_apply_wide(sig, elec_nine, alpha = 0.05,
+                        tws = time_windows)
+        sigcols <- grepl("_sig", colnames(sig_corr))
+        tval <- cbind(tval, sig_corr[, ..sigcols])
+        tval$Condition <- tval$Spec
+        plot_nine_elec(tval, elec_nine,
+            file = paste0("../plots/", file, "/Waveforms/t-values.pdf"),
+            title = "Inferential Statistics",
+            modus = "t-value", ylims = c(8, -9), tws = time_windows,
+            leg_labs = model_labs[2:length(model_labs)],
+            leg_vals = model_vals[2:length(model_vals)])
+    }
 
     ########
     # DATA #
-    # ########
+    ########
     eeg <- fread(paste0("../data/", file, "_data.csv"))
 
-    # eeg$Condition <- factor(plyr::mapvalues(eeg$Condition, c(1, 2),
-    #                 c("A", "C")), levels = c("A", "C"))
-    # data_labs <- c("A", "C")
-    # data_vals <- c("black", "#004488")
-
-    # eeg$Condition <- factor(plyr::mapvalues(eeg$Condition, c(4, 1, 3, 2),
-    #                  c("B", "A", "C", "D")), levels = c("A", "B", "C", "D"))
-    # data_labs <- c("A", "B", "C", "D")
-    # data_vals <- c("#000000", "#BB5566", "#004488", "#DDAA33")
-
-    eeg$Condition <- factor(plyr::mapvalues(eeg$Condition, c(2, 1, 3),
-                     c("B", "A", "C")), levels = c("A", "B", "C"))
-    data_labs <- c("A", "B", "C")
-    data_vals <- c("#000000", "red", "blue")
+    if (design == "Design1_AC") {
+        eeg$Condition <- factor(plyr::mapvalues(eeg$Condition, c(1, 2),
+                        c("A", "C")), levels = c("A", "C"))
+        data_labs <- c("A", "C")
+        data_vals <- c("black", "#004488")
+    } else if (design == "Design1") {
+        eeg$Condition <- factor(plyr::mapvalues(eeg$Condition, c(2, 1, 3, 4),
+                     c("B", "A", "C", "D")), levels = c("A", "B", "C", "D"))
+        data_labs <- c("A", "B", "C", "D")
+        data_vals <- c("#000000", "#BB5566", "#004488", "#DDAA33")
+    } else if (design == "Design2") {
+        eeg$Condition <- factor(plyr::mapvalues(eeg$Condition, c(2, 1, 3),
+                        c("B", "A", "C")), levels = c("A", "B", "C"))
+        data_labs <- c("A", "B", "C")
+        data_vals <- c("#000000", "red", "blue")
+    }
 
     # Data: Observed
-    obs <- eeg[Type == "EEG",]
+    obs <- eeg[Type == "EEG", ]
+
     plot_single_elec(obs, c("Pz"),
         file = paste0("../plots/", file,  "/Waveforms/Observed.pdf"),
         title = "Observed",
@@ -87,37 +93,35 @@ make_plots <- function(
         title = "Observed", modus = "Condition",
         ylims = c(9, -5.5), leg_labs = data_labs, leg_vals = data_vals)
 
-    plot_topo(obs, file = paste0("../plots/", file, "/Topos/Observed"),
-                tw = c(600, 1000), cond_man = "B", cond_base = "A",
-                add_title = "\nObserved",
-                omit_legend = TRUE, save_legend = TRUE)
-    plot_topo(obs, file = paste0("../plots/", file, "/Topos/Observed"),
+    if (design == "Design1") {
+        plot_topo(obs, file = paste0("../plots/", file, "/Topos/Observed"),
                 tw = c(300, 500), cond_man = "C", cond_base = "A",
                 omit_legend = TRUE, save_legend = TRUE)
+    }
 
     # Data: Estimated
     combo <- c("Intercept", "Intercept + Cloze", "Intercept + Noun Association",
                 "Intercept + Cloze + Noun Association")
     pred <- c("Intercept", "Tar. Plaus.", "Dist. Cloze",
               "Dist. Cloze + Tar. Plaus.")
-    est <- eeg[Type == "est",]
+    est <- eeg[Type == "est", ]
     for (i in seq(1, length(unique(est$Spec)))) {
         spec <- unique(est$Spec)[i]
         est_set <- est[Spec == spec, ]
         spec <- unique(est_set$Spec)
         name <- gsub("\\[|\\]|:|,| ", "", spec)
         plot_single_elec(est_set, c("Pz"),
-            file = paste0("../plots/", file, "/Waveforms/Estimated_", name, ".pdf"),
+            file = paste0("../plots/", file,
+                "/Waveforms/Estimated_", name, ".pdf"),
             title = paste("Estimates", combo[i]),
             modus = "Condition", ylims = c(9, -5.5),
             leg_labs = data_labs, leg_vals = data_vals)
-        plot_topo(est_set,
+        if (design == "Design2") {
+            plot_topo(est_set,
             file = paste0("../plots/", file, "/Topos/Estimated_", name),
             tw = c(600, 1000), cond_man = "B", cond_base = "A",
             add_title = paste("\nEstimate", pred[i]), omit_legend = TRUE)
-        plot_topo(est_set,
-            file = paste0("../plots/", file, "/Topos/Estimated_", name),
-            tw = c(300, 500), cond_man = "C", cond_base = "A")
+        }
     }
 
     # Data: Residual
@@ -132,9 +136,12 @@ make_plots <- function(
                  name, ".pdf"), title = paste("Residuals", combo[i]),
                  modus = "Condition", ylims = c(3.5, -3.5),
                  leg_labs = data_labs, leg_vals = data_vals)
-        plot_topo(res_set, 
+        if (design == "Design2") {
+            plot_topo(res_set,
                 file = paste0("../plots/", file, "/Topos/Residual_", name),
-                tw = c(300, 500), cond_man = "C", cond_base = "A")
+                tw = c(600, 1000), cond_man = "B", cond_base = "A",
+                add_title = paste("\nResidual", pred[i]), omit_legend = TRUE)
+        }
     }
 }
 
@@ -142,20 +149,27 @@ elec_all <- c("Fp1", "Fp2", "F7", "F3", "Fz", "F4", "F8", "FC5",
                 "FC1", "FC2", "FC6", "C3", "Cz", "C4", "CP5", "CP1",
                 "CP2", "CP6", "P7", "P3", "Pz", "P4", "P8", "O1", "Oz", "O2")
 
+make_plots("ERP_Design1_AC_Intercept_rERP", elec_all,
+    predictor = c("Intercept"), design = "Design1_AC",
+    model_labs = c("Intercept"))
 
-#make_plots("ERP_Design1_AC_Intercept_rERP", predictor = c("Intercept"))
+make_plots("ERP_Design1_AC_CondCode_rERP", elec_all,
+    predictor = c("Intercept", "CondCode"), design = "Design1_AC",
+    model_labs = c("Intercept", "Condition Code"))
 
-#make_plots("ERP_Design1_AC_CondCode_rERP",
-#    predictor = c("Intercept", "CondCode"))
+make_plots("ERP_Design1_AC_cloze_rERP", elec_all,
+    predictor = c("Intercept", "Cloze"), design = "Design1_AC",
+    model_labs = c("Intercept", "Cloze"))
 
-#make_plots("ERP_Design1_AC_cloze_rERP", elec_all,
-#    predictor = c("Intercept", "Cloze"))
+make_plots("ERP_Design1_cloze_rcnoun_rERP", elec_all,
+    predictor = c("Intercept", "Cloze", "rcnoun"), design = "Design1",
+    model_labs = c("Intercept", "Cloze", "Noun Association"))
 
-#make_plots("ERP_Design1_cloze_rcnoun_rERP", elec_all,
-#    predictor = c("Intercept", "Cloze", "rcnoun"))
+make_plots("ERP_Design1_cloze_rcnoun_across_rERP", elec_all,
+    predictor = c("Intercept", "Cloze", "rcnoun"),
+    inferential = TRUE, design = "Design1",
+    model_labs = c("Intercept", "Cloze", "Noun Association"))
 
-# make_plots("ERP_Design1_AC_cloze_rERP", elec_all,
-#     predictor = c("Intercept", "Cloze"))
-
-make_plots("ERP_Design2_Plaus_Clozedist", elec_all,
-    predictor = c("Intercept", "Plaus", "Cloze_distractor"))
+make_plots("ERP_Design2_Plaus_Clozedist_rERP", elec_all,
+    predictor = c("Intercept", "Plaus", "Cloze_distractor"), design = "Design2",
+    model_labs = c("Intercept", "Plausibility", "Distractor Cloze"))
