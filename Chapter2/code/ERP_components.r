62330de022fa73d493959f438fb724ba725d163c
caurnhammer <chr.aurnhammer@gmail.com>
2023-03-16 01:34:58 +0100

    Ch2 cleanup

--- Chapter2/code/ERP_components.r
+++ Chapter2/code/ERP_components.r
@@ -31,17 +31,21 @@ p <- p + annotate("text", x = 400, y = ylimits[2], label = "N400")
 p <- p + annotate("rect", xmin = 600, xmax = 1000, ymin = ylimits[2],
             ymax = ylimits[1], alpha = .15)
 p <- p + annotate("text", x = 800, y = ylimits[2], label = "P600")
-
-gg <- p + theme(legend.key.size = unit(0.5, "cm"), #change legend key size
+p <- p + theme(legend.key.size = unit(0.5, "cm"), #change legend key size
         legend.key.height = unit(0.5, "cm"), #change legend key height
         legend.key.width = unit(0.5, "cm"), #change legend key width
         legend.title = element_text(size = 9), #change legend title font size
         legend.text = element_text(size = 7))
-gg <- gg + theme(plot.title = element_text(size = 7.5))
-legend <- get_legend(gg)
+p <- p + theme(plot.title = element_text(size = 7.5))
+
+legend <- get_legend(p)
 nl <- theme(legend.position = "none")
-gg <- arrangeGrob(gg + nl + ggtitle("Pz"),
+p <- arrangeGrob(p + nl + ggtitle("Pz"),
             legend, heights = c(10, 1))
 
-ggsave(file = "../plots/ERP_Design1_AC_Pz.pdf", gg,
+ggsave(file = "../Figures/ERP_Design1_AC_Pz.pdf", p,
     device = cairo_pdf, width = 3, height = 3)
+
+if (file.exists("Rplots.pdf")){
+        file.remove(("Rplots.pdf"))
+}
