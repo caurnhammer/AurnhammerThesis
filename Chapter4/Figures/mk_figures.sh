6b7c1c7f9447f2c45131f3a939c6f80b5d5fd4b3
caurnhammer <chr.aurnhammer@gmail.com>
2023-03-17 23:42:24 +0100

    pdfjam

--- Chapter4/Figures/mk_figures.sh
+++ Chapter4/Figures/mk_figures.sh
@@ -1,99 +1,138 @@
-# Check if tmp dir exists
-mkdir -p tmp Stimuli SPR ERP
+## Check if tmp dir exists
+mkdir -p tmp  Stimuli SPR ERP
 
 ## Stimuli
-cp ../Stimuli/adbc23_Densities.pdf Stimuli_Densities.pdf
+cp ../Stimuli/Design2_Densities.pdf Stimuli/Design2_Densities.pdf
 
 ## SPR
-# convert -density 500 \
-# 		../rRTs_Plaus_Clozedist/RT_logRT.pdf \
-# 		../rRTs_Plaus_Clozedist/RT_logRT_wavelegend.pdf \
-# 		-append \
-# 		RT_logRT.pdf
+# RT_logRT
+cp ../SPR/plots/rRT_Plaus_Clozedist/RT_logRT.pdf  SPR/RT_logRT.pdf
 
-# convert -density 500 \
-# 		../rRTs_Plaus_Clozedist/RT_est_PlausCloze_distractor.pdf \
-# 		../rRTs_Plaus_Clozedist/RT_res_PlausCloze_distractor.pdf \
-# 		+append -page 1800x900 \
-# 		tmp/RT_est_res_PlausClozedist.pdf
+# RT_est_res_PlausClozedist
+pdfjam 	../SPR/plots/rRT_Plaus_Clozedist/RT_est_PlausCloze_distractor.pdf \
+	   	../SPR/plots/rRT_Plaus_Clozedist/RT_res_PlausCloze_distractor.pdf \
+		--nup 2x1 --landscape \
+		--outfile tmp/RT_est_res_PlausClozedist.pdf \
+		--papersize '{8cm,15cm}'
+pdfjam	--scale 1 \
+		../SPR/plots/rRT_Plaus_Clozedist/RT_est_1_rtlegend.pdf \
+		--landscape \
+		--outfile tmp/RT_est_1_rtlegend.pdf \
+		--papersize '{1cm,15cm}'
+pdfjam 	tmp/RT_est_res_PlausClozedist.pdf \
+		tmp/RT_est_1_rtlegend.pdf \
+		--delta '0 -80' --nup 1x2 --landscape \
+		--outfile tmp/RT_est_res_PlausClozedist_2.pdf \
+		--papersize '{9cm,15cm}' 
+pdfjam 	tmp/RT_est_res_PlausClozedist_2.pdf \
+		--landscape \
+		--outfile SPR/RT_est_res_PlausClozedist.pdf \
+		--papersize '{9cm,15cm}' \
+		--trim '2cm 3.5cm 2cm 1cm' 
 
-# convert	-density 2000 \
-# 	tmp/RT_est_res_PlausClozedist.pdf \
-# 	../rRTs_Plaus_Clozedist/RT_logRT_wavelegend.pdf \
-# 	-append -page 400x250 \
-# 	RT_est_res_PlausClozedist.pdf
+# RT_coefficients_zvalue
+pdfjam ../SPR/plots/rRT_Plaus_Clozedist/RT_coefficients.pdf \
+	   ../SPR/plots/rRT_Plaus_Clozedist/RT_zvalue.pdf \
+	   --nup 2x1 --landscape \
+	   --outfile SPR/RT_coefficients_zvalue.pdf \
+	   --papersize '{8cm,15cm}'
 
-# convert	-density 1000 \
-# 	../rRTs_Plaus_Clozedist/RT_coefficients.pdf \
-# 	../rRTs_Plaus_Clozedist/RT_zvalue.pdf \
-# 	+append -page 400x200 \
-# 	RT_coefficients_zvalue.pdf
-
-# convert	-density 1000 \
-# 	../rRTs_PrecritRT_Plaus_Clozedist/RT_coefficients.pdf \
-# 	../rRTs_PrecritRT_Plaus_Clozedist/RT_zvalue.pdf \
-# 	+append -page 400x200 \
-# 	RT_Precrit_coefficients_zvalue.pdf
+# RT_Precrit_coefficients_zvalue
+pdfjam 	../SPR/plots/rRT_PrecritRT_Plaus_Clozedist/RT_coefficients.pdf \
+		../SPR/plots/rRT_PrecritRT_Plaus_Clozedist/RT_zvalue.pdf \
+		--nup 2x1 --landscape \
+	   	--outfile SPR/RT_Precrit_coefficients_zvalue.pdf \
+		--papersize '{8cm,15cm}'
 
 ## ERP
-cp ../ERP/plots/Design2_Plaus_Clozedist/Waveforms/Observed_Full.pdf 	ERP/ERP_Observed_Full.pdf
-cp ../ERP/plots/Design2_Plaus_Clozedist_across/Waveforms/t-values.pdf 	ERP/ERP_across_tvalues.pdf
-cp ../ERP/plots/Design2_ReadingTime/Waveforms/Coefficients_Pz.pdf 		ERP/ERP_RT.pdf
+# ERP_Observed_Full
+cp ../ERP/plots/rERP_Plaus_Clozedist/Waveforms/Observed_Full.pdf ERP/ERP_Observed_Full.pdf
 
-convert -density 500 \
-	../ERP/plots/Design2_Plaus_Clozedist/Waveforms/EstimatedPz_InterceptPlausCloze_distractor.pdf \
-	../ERP/plots/Design2_Plaus_Clozedist/Waveforms/ResidualPz_InterceptPlausCloze_distractor.pdf \
-	+append -page 1680:840 \
-	tmp/ERP_EstRes_Pz.pdf
-convert -density 2000 \
-	tmp/ERP_EstRes_Pz.pdf \
-	../ERP/plots/Design2_Plaus_Clozedist/Waveforms/EstimatedPz_InterceptPlausCloze_distractor_wavelegend.pdf \
-	-append -page 800:500 \
-	ERP/ERP_EstRes_Pz.pdf
+# Observed_Topos_B
+pdfjam 	--scale 1.5 ../ERP/plots/rERP_Plaus_Clozedist/Topos/Observed_topolegend.pdf \
+		--outfile tmp/Observed_topolegend.pdf
+pdfjam 	../ERP/plots/rERP_Plaus_Clozedist/Topos/Observed_B_250-400.pdf \
+		../ERP/plots/rERP_Plaus_Clozedist/Topos/Observed_B_300-500.pdf \
+		../ERP/plots/rERP_Plaus_Clozedist/Topos/Observed_B_600-1000.pdf \
+		tmp/Observed_topolegend.pdf \
+		--delta '-20 0' --nup 4x1 --landscape \
+		--outfile ERP/ERP_Observed_Topos_B.pdf \
+		--papersize '{8cm,24cm}'
 
-convert -density 500 \
-	../ERP/plots/Design2_Plaus_Clozedist/Waveforms/Coefficients_C3.pdf  \
-	../ERP/plots/Design2_Plaus_Clozedist/Waveforms/Coefficients_Pz.pdf \
-	+append -page 1680:840 \
-	ERP/tmp/ERP_Coef_C3Pz.pdf
-convert -density 2000 \
-	tmp/ERP_Coef_C3Pz.pdf \
-	../ERP/plots/Design2_Plaus_Clozedist/Waveforms/Coefficients_C3_wavelegend.pdf \
-	-append -page 800:500 \
-	ERP/ERP_Coef_C3Pz.pdf
+# Observed_Topos_C
+pdfjam 	../ERP/plots/rERP_Plaus_Clozedist/Topos/Observed_C_250-400.pdf \
+		../ERP/plots/rERP_Plaus_Clozedist/Topos/Observed_C_300-500.pdf \
+		../ERP/plots/rERP_Plaus_Clozedist/Topos/Observed_C_600-1000.pdf \
+		tmp/Observed_topolegend.pdf \
+		--delta '-20 0' --nup 4x1 --landscape \
+		--outfile ERP/ERP_Observed_Topos_C.pdf \
+		--papersize '{8cm,24cm}'
 
-convert -density 500 \
-	../ERP/plots/Design2_Plaus_Clozedist/Waveforms/EstimatedPz_InterceptCloze_distractor.pdf \
-	../ERP/plots/Design2_Plaus_Clozedist/Waveforms/EstimatedPz_InterceptPlaus.pdf \
-	+append -page 1680:840 \
-	tmp/ERP_Est_Iso_Pz.pdf
-convert -density 2000 \
-	tmp/ERP_Est_Iso_Pz.pdf \
-	../ERP/plots/Design2_Plaus_Clozedist/Waveforms/EstimatedPz_InterceptPlausCloze_distractor_wavelegend.pdf \
-	-append -page 800:500 \
-	ERP/ERP_Est_Iso_Pz.pdf
+# ERP_EstRes_Pz
+pdfjam  ../ERP/plots/rERP_Plaus_Clozedist/Waveforms/EstimatedPz_InterceptPlausCloze_distractor.pdf \
+		../ERP/plots/rERP_Plaus_Clozedist/Waveforms/ResidualPz_InterceptPlausCloze_distractor.pdf \
+		--nup 2x1 --landscape \
+		--outfile tmp/ERP_EstRes_Pz.pdf \
+		--papersize '{8cm,15cm}'
+pdfjam 	tmp/ERP_EstRes_Pz.pdf \
+		../ERP/plots/rERP_Plaus_Clozedist/Waveforms/EstimatedPz_InterceptPlausCloze_distractor_wavelegend.pdf \
+		--delta '0 -80' --nup 1x2 --landscape \
+		--outfile ERP/ERP_EstRes_Pz.pdf \
+		--papersize '{9cm,15cm}' \
+		--trim '2cm 2.5cm 1cm 0cm'
 
-convert -density 250 ../ERP/plots/Design2_Plaus_Clozedist/Topos/Observed_B_250-400.pdf \
-	../ERP/plots/Design2_Plaus_Clozedist/Topos/Observed_B_300-500.pdf \
-    ../ERP/plots/Design2_Plaus_Clozedist/Topos/Observed_B_600-1000.pdf \
-	../ERP/plots/Design2_Plaus_Clozedist/Topos/Observed_topolegend.pdf \
-	+append -page 400:130 \
-	ERP/ERP_Observed_Topos_B.pdf
+# ERP_Coef_C3Pz
+pdfjam 	../ERP/plots/rERP_Plaus_Clozedist/Waveforms/Coefficients_C3.pdf \
+		../ERP/plots/rERP_Plaus_Clozedist/Waveforms/Coefficients_Pz.pdf \
+		--nup 2x1 --landscape \
+		--outfile tmp/ERP_Coef_C3Pz.pdf \
+		--papersize '{8cm,15cm}'
+pdfjam 	--scale 1.5 \
+		../ERP/plots/rERP_Plaus_Clozedist/Waveforms/Coefficients_C3_wavelegend.pdf \
+		--landscape \
+		--outfile tmp/Coefficients_C3_wavelegend.pdf \
+		--papersize '{1cm,15cm}'
+pdfjam  tmp/ERP_Coef_C3PZ.pdf \
+		tmp/Coefficients_C3_wavelegend.pdf \
+		--delta '0 -80' --nup 1x2 --landscape \
+		--outfile tmp/ERP_Coef_C3Pz_2.pdf \
+		--papersize '{9cm,15cm}' 
+pdfjam 	tmp/ERP_Coef_C3Pz_2.pdf \
+		--landscape \
+		--outfile ERP/ERP_Coef_C3Pz.pdf \
+		--papersize '{9cm,15cm}' \
+		--trim '2cm 2.5cm 2cm 0cm'
 
-convert -density 250 ../ERP/plots/Design2_Plaus_Clozedist/Topos/Observed_C_250-400.pdf \
-	../ERP/plots/Design2_Plaus_Clozedist/Topos/Observed_C_300-500.pdf \
-    ../ERP/plots/Design2_Plaus_Clozedist/Topos/Observed_C_600-1000.pdf \
-	../ERP/plots/Design2_Plaus_Clozedist/Topos/Observed_topolegend.pdf \
-	+append -page 400:130 \
-	ERP/ERP_Observed_Topos_C.pdf
+# ERP_Est_Iso_Pz
+pdfjam 	../ERP/plots/rERP_Plaus_Clozedist/Waveforms/EstimatedPz_InterceptCloze_distractor.pdf \
+	 	../ERP/plots/rERP_Plaus_Clozedist/Waveforms/EstimatedPz_InterceptPlaus.pdf \
+		--nup 2x1 --landscape \
+		--outfile tmp/ERP_Est_Iso_Pz.pdf \
+		--papersize '{8cm,15cm}'
 
-convert -density 250 ../ERP/plots/Design2_Plaus_Clozedist/Topos/Estimated_InterceptCloze_distractor_B_600-1000.pdf \
-    ../ERP/plots/Design2_Plaus_Clozedist/Topos/Estimated_InterceptPlaus_B_600-1000.pdf \
-	../ERP/plots/Design2_Plaus_Clozedist/Topos/Estimated_InterceptPlausCloze_distractor_B_600-1000.pdf \
-    ../ERP/plots/Design2_Plaus_Clozedist/Topos/Observed_B_600-1000.pdf \
-	../ERP/plots/Design2_Plaus_Clozedist/Topos/Observed_topolegend.pdf \
-	+append -page 350:80 \
-	ERP/ERP_Estimated_Topos_B.pdf
+pdfjam  tmp/ERP_Est_Iso_Pz.pdf \
+		../ERP/plots/rERP_Plaus_Clozedist/Waveforms/EstimatedPz_InterceptPlausCloze_distractor_wavelegend.pdf \
+		--delta '0 -80' --nup 1x2 --landscape \
+		--outfile ERP/ERP_Est_Iso_Pz.pdf \
+		--papersize '{9cm,15cm}' \
+		--trim '2cm 2.5cm 1cm 0cm'
 
-# clean up
+# ERP_Estimated_Topos_B
+pdfjam 	../ERP/plots/rERP_Plaus_Clozedist/Topos/Estimated_InterceptCloze_distractor_B_600-1000.pdf \
+		../ERP/plots/rERP_Plaus_Clozedist/Topos/Estimated_InterceptPlaus_B_600-1000.pdf \
+		../ERP/plots/rERP_Plaus_Clozedist/Topos/Estimated_InterceptPlausCloze_distractor_B_600-1000.pdf \
+		../ERP/plots/rERP_Plaus_Clozedist/Topos/Observed_B_600-1000.pdf \
+		tmp/Observed_topolegend.pdf \
+		--delta '-20 0' --nup 5x1 \
+		--landscape \
+		--outfile ERP/ERP_Estimated_Topos_B.pdf \
+		--papersize '{8cm,32cm}'
+
+# ERP_across_tvalues
+cp ../ERP/plots/rERP_Plaus_Clozedist_across/Waveforms/t-values.pdf ERP/ERP_across_tvalues.pdf
+
+# ERP_RT
+cp ../ERP/plots/rERP_RT/Waveforms/Coefficients_Pz.pdf ERP/ERP_RT.pdf
+
+## remove tmp
 rm -r tmp
